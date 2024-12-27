0b097daca21694de4c528312ec5d0ee2072f9dfc
Khant Naing Set <khantnaingset@onenex.co>
2024-12-27 22:58:29 +0700

    add k8s provider

--- providers.tf
+++ providers.tf
@@ -4,5 +4,5 @@ provider "aws" {
 
 provider "kubernetes" {
   host = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
